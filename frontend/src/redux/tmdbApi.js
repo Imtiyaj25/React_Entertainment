03d0855744325a53e0dc82cb080a29b8f8837e79
Imtiyaj <129411789+Imtiyaj25@users.noreply.github.com>
2024-02-15 15:40:44 +0530

    Updating api

--- frontend/src/redux/tmdbApi.js
+++ frontend/src/redux/tmdbApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://react-entertainment-backend.onrender.com/api" }),
   endpoints: (builder) => ({
     getTrending: builder.query({
       query: (type) =>
